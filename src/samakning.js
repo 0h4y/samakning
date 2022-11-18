b9dd096fdb2a4a323adddc0363bb52fe2075d91b
ShoulderEscape <105722638+ShoulderEscape@users.noreply.github.com>
2022-11-18 13:28:34 +0100

    Lagt till funktion för min ålder 18 år bokning

--- src/samakning.js
+++ src/samakning.js
@@ -4,8 +4,18 @@ Hej
 
 Hej från Martin L
 */
+
+//Kör när html laddar
 window.onload =function(){
+
+    //Får dagens datum år
     let date=new Date();
-    let maxdate=new Date(date.getFullYear()-18, date.getMonth(),date.getDate());
-    document.getElementById("fodelsedatum").setAttribute("max", maxdate );
+    //dagens datum -18 år, och +1 månad för Date() objected listar månader från 0-11, medans setAtrribute(max) tar in det från 1-12
+    let maxdate=new Date(date.getFullYear()-18, date.getMonth()+1,date.getDate());
+    //Konverterar till sträng, för att det är så setAttribute vill ha det på sitt andra argument
+    let maxdateStr=""+maxdate.getFullYear()+"-"+maxdate.getMonth()+"-"+maxdate.getDate();
+    //Hittar födelsedatum väljaren, och gör om det till en variabel som man kan använda för saker likt setAttribute
+    let fodelsedatum=document.getElementById("fodelsedatum");
+    //Gör om Minimum åldern av användarna till 18
+    fodelsedatum.setAttribute("max", maxdateStr );
 }
