4fcd4c13a5ffdbcc0e5276a853f62e1a4f46a653
ShoulderEscape <105722638+ShoulderEscape@users.noreply.github.com>
2022-11-18 12:36:14 +0100

    Lägger till maxdate

--- src/samakning.js
+++ src/samakning.js
@@ -4,3 +4,8 @@ Hej
 
 Hej från Martin L
 */
+window.onload =function(){
+    let date=new Date();
+    let maxdate=new Date(date.getFullYear()-18, date.getMonth(),date.getDate());
+    document.getElementById("fodelsedatum").setAttribute("max", maxdate );
+}
