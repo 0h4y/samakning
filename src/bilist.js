0b9df8292a8d37afdffe43fe1aefe27149e296d7
MartKarl <Martin.Karlsson95@hotmail.com>
2022-11-21 14:00:32 +0100

    Js för formen avgångs datum och hemgångs datum så man bara kan välja datum från dagens datum och framåt

--- src/bilist.js
+++ src/bilist.js
@@ -1,3 +1,35 @@
 /*Javascript för bilist nedan...
 Grupp, mattias, savio, martin K
 */
+
+//Lägger in dagens datum i currentDateTime
+let currentDateTime = new Date();
+//variabel som innehåller dagens år
+let year = currentDateTime.getFullYear();
+//variabel som innehåller dagens månad, getMonth är 0 index based
+let month = (currentDateTime.getMonth() + 1);
+//variabel som innehåller dagens dag, getDate är 0 index based
+let date = (currentDateTime.getDate());
+
+//Skriver ut siffran 0 så datum alltid innehåller 2 siffror
+if(date < 10) {
+  date = '0' + date;
+}
+
+if(month < 10) {
+  month = '0' + month;
+}
+//dateTomorrow == dagens datum
+let dateTomorrow = year + "-" + month + "-" + date;
+//Tar alltid ut första elementet
+let avgångElem = document.querySelector("#avgång-datum");
+let hemgångElem = document.querySelector("#hemgång-datum");
+
+//sätter ett minimum på dagens datum
+avgångElem.setAttribute("min", dateTomorrow);
+
+//Metod för avgångsdadum
+avgångElem.onchange = function () {
+    //Begränsar hemgång till datumet man väljer i avgång. this.Value är värdet från #avgångs-datum
+    hemgångElem.setAttribute("min", this.value);
+}
